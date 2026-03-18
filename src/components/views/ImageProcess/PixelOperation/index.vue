0298d23472a5ffcd9c770c7f22a33ed984d83ac6
csy2023150048 <257237967@qq.com>
2026-03-18 18:37:30 +0800

    feat/添加图像色彩转换逻辑

--- src/components/views/ImageProcess/PixelOperation/index.vue
+++ src/components/views/ImageProcess/PixelOperation/index.vue
@@ -3,22 +3,37 @@
  * @Author: 陈思宇
  * @Date: 2026-03-18 14:24:00
  * @LastEditors: 陈思宇
- * @LastEditTime: 2026-03-18 14:34:00
+ * @LastEditTime: 2026-03-18 18:10:00
  */
  <template>
   <div class="workplace">
-    <canvas-wrapper v-model:has-image="isloaded" @update:has-image="handleUpload" ref="inputCanvas"/>
+    <canvas-wrapper v-model:has-image="isloaded" @update:has-image="handleUpload" ref="inputCanvas" @ready = "onCanvasReady"/>
   </div>
  </template>
  
  <script setup lang="ts">
 import CanvasWrapper from '@/components/common/CanvasWrapper.vue';
+import { useColorConversionStore } from '@/store/picture/PixelOperation/ColorConversion.ts/colorConversion';
+import { usePictureStore } from '@/store/picture/picture';
 import { ref } from 'vue';
+
+const colorConversionStore = useColorConversionStore();
+const pictureStore = usePictureStore();
 
 let isloaded = ref<boolean>(false);
 
-function handleUpload(){
+const inputCanvas = ref<InstanceType<typeof CanvasWrapper> | null>(null);
+function handleUpload(val: boolean){
+  if(!val) return;
+  isloaded.value = true;
+  let canvas = inputCanvas.value?.getCanvas();
+  if(canvas)
+    colorConversionStore.setInputCanvas(canvas);
+}
 
+const onCanvasReady = (data: {visualWidth: number, visualHeight: number, baseRatio: number}) => {
+  pictureStore.setSize(data.visualWidth, data.visualHeight);
+  pictureStore.setBaseRatio(data.baseRatio);
 }
 </script>
 
